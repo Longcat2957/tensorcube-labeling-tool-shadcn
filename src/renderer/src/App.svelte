514a8b1bbdd708bc5f0dec6354215ae16e04213a
Longcat2957 <schrodingers030@gmail.com>
2026-03-04 18:34:46 +0900

    init

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+ import { Button } from "$lib/components/ui/button/index.js";
+</script>
+<Button>Button</Button>
+<Button variant="outline">Outline</Button>
+<Button variant="ghost">Ghost</Button>
+<Button variant="destructive">Destructive</Button>
